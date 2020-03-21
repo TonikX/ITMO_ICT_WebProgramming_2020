6a6cbb3adbe0f37534322c076182be756dfc604c
18ariana <notwildariadna@gmail.com>
2020-03-21 16:37:14 +0300

    add first practice work

---
+++ students/k3343/practical_works/Tsybaeva_Arina/first_django_app/django_project_tsybaeva/project_first_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import Http404
+from .models import Owner
+
+def detail(request, owner_id):
+    try:
+        owner = Owner.objects.get(pk=owner_id)
+    except Owner.DoesNotExist:
+        raise Http404("Owner does not exist")
+    return render(request, 'owner.html', {'owner': owner})

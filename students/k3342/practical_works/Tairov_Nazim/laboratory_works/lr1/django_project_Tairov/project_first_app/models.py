43a242c8acf293fcd4946ab322bc5e372634f041
Nazim Tairov <n.tairov9777@gmail.com>
2020-03-21 14:45:45 +0300

    fix

--- students/k3342/practical_works/Tairov_Nazim/laboratory_works/lr1/django_project_Tairov/project_first_app/models.py
+++ students/k3342/practical_works/Tairov_Nazim/laboratory_works/lr1/django_project_Tairov/project_first_app/models.py
@@ -10,22 +10,6 @@ class CarOwner(models.Model):
         return f'User {self.name}'
 
 
-class DriverLicense(models.Model):
-    TYPE_CHOICES = (('A', 'Bike'),
-                    ('B', 'Light Rigid'),
-                    ('C', 'Medium Rigid'),
-                    ('D', 'Heavy Rigid'),
-                    ('E', 'Heavy Combination'))
-
-    license_id = models.PositiveIntegerField()
-    date_of_issue = models.DateField(blank=True, null=True)
-    type = models.CharField(max_length=5, choices=TYPE_CHOICES)
-    owner = models.ForeignKey(CarOwner, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.license_id}'
-
-
 class Car(models.Model):
     car_brand = models.CharField(max_length=50)
     car_model = models.CharField(max_length=50)
@@ -37,11 +21,27 @@ class Car(models.Model):
         return f'{self.car_brand}'
 
 
+class DriverLicense(models.Model):
+    TYPE_CHOICES = (('A', 'Bike'),
+                    ('B', 'Light Rigid'),
+                    ('C', 'Medium Rigid'),
+                    ('D', 'Heavy Rigid'),
+                    ('E', 'Heavy Combination'))
+
+    license_id = models.PositiveIntegerField()
+    date_of_issue = models.DateField(blank=True, null=True)
+    choice_type = models.CharField(max_length=5, choices=TYPE_CHOICES)
+    owner = models.ManyToManyField(Car, through='TemporaryOwner', related_name='temp_owner')
+
+    def __str__(self):
+        return f'{self.license_id}'
+
+
 class TemporaryOwner(models.Model):
-    car_owner = models.ForeignKey(CarOwner, on_delete=models.CASCADE)
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    car_owner = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name='Car')
+    driver_license = models.ForeignKey(DriverLicense, on_delete=models.CASCADE)
     date_started = models.DateTimeField(blank=True, null=True)
     date_ended = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
-        return f'{self.car_owner} owns {self.car}'
+        return f'{self.car_owner} owned by user with license id {self.driver_license}'

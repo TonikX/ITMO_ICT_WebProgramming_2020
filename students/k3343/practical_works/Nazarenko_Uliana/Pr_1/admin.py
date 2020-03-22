f18e3eeb7440fcead73d65f08cf041577aff698c
Uliana Nazarenko <31666147+Ylikn@users.noreply.github.com>
2020-03-23 01:49:02 +0300

    Delete admin.py

--- students/k3343/practical_works/Nazarenko_Uliana/Pr_1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Car_owner, Car, Ownership, Drivers_lic
-
-admin.site.register(Car_owner)
-admin.site.register(Car)
-admin.site.register(Ownership)
-admin.site.register(Drivers_lic)

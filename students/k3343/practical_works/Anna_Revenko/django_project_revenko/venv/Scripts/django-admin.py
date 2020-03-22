3eca1c23320927cf0173f3b84df5ff993e16a119
annier <annarevenko3191@yandex.ru>
2020-03-23 00:54:48 +0300

    1 практическая работа

---
+++ students/k3343/practical_works/Anna_Revenko/django_project_revenko/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\810314\PycharmProjects\django_project_revenko\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

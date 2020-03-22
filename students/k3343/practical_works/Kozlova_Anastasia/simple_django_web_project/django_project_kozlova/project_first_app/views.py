21d921e9a7d0ee7d080e264138d0c477b25c0049
stasiakozlova <stasia.kozlova@gmail.com>
2020-03-22 23:09:20 +0300

    Сделана первая практическая

---
+++ students/k3343/practical_works/Kozlova_Anastasia/simple_django_web_project/django_project_kozlova/project_first_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from project_first_app.models import Owner
+from django.http import Http404
+
+
+# Create your views here.
+def owner_info(request, owner_id):
+    try:
+        owner = Owner.objects.get(pk=owner_id)
+    except Owner.DoesNotExist:
+        raise Http404("Owner does not exist")
+
+    return render(request, 'owner.html', {'owner': owner})

15be7cec054ed5d77859b691c75e5583f5705989
Marina Berezhnova <7marinabe@gmail.com>
2020-03-28 19:10:44 +0300

    Сделана вторая практическая работа

--- students/k3343/practical_works/Berezhnova_Marina/simple_django_web_project/django_project_berezhnova/project_first_app/urls.py
+++ students/k3343/practical_works/Berezhnova_Marina/simple_django_web_project/django_project_berezhnova/project_first_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-        path('owner/<int:driver_id>', views.get_driver)
+        path('owner/<int:driver_id>', views.get_driver),
+        path('auto/<int:auto_id>', views.CarView.as_view())
 ]
--- students/k3343/practical_works/Berezhnova_Marina/simple_django_web_project/django_project_berezhnova/project_first_app/views.py
+++ students/k3343/practical_works/Berezhnova_Marina/simple_django_web_project/django_project_berezhnova/project_first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from project_first_app.models import Driver
+from project_first_app.models import Driver, Auto
 from django.http import Http404
+from django.views import View
 
 
 # Create your views here.
@@ -11,3 +12,13 @@ def get_driver(request, driver_id):
         raise Http404("Owner does not exist")
 
     return render(request, 'driver.html', {'driver': driver})
+
+
+class CarView(View):
+    def get(self, request, auto_id):
+        try:
+            car = Auto.objects.get(pk=auto_id)
+        except Auto.DoesNotExist:
+            raise Http404("Car does not exist")
+ 
+        return render(request, 'car.html', {'car': car})

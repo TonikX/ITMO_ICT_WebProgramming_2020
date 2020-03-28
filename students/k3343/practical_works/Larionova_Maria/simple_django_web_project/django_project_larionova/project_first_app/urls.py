30138ee481bc528e80f7d587218e94131692a724
Marika Larionova <marialarionova@gmail.com>
2020-03-28 19:30:54 +0300

    Вторая практическая работа

--- students/k3343/practical_works/Larionova_Maria/simple_django_web_project/django_project_larionova/project_first_app/urls.py
+++ students/k3343/practical_works/Larionova_Maria/simple_django_web_project/django_project_larionova/project_first_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-        path('owner/<int:owner_id>', views.get_owner_info)
+        path('owner/<int:owner_id>', views.get_owner_info),
+        path('auto/<int:auto_id>', views.AutoInfoView.as_view())
 ]

c009949e61e63be07c669f091aa462b227427d8a
Salnikova Nadezda <uninadia@yandex.ru>
2020-04-04 17:23:01 +0300

    Выполнение 3 практической работы. Созданы формы, представления, шаблоны и отдельные url-адреса.

--- students/k3342/practical_works/Salnikova_Nadezhda/simple_django_web_project/project_first_app/urls.py
+++ students/k3342/practical_works/Salnikova_Nadezhda/simple_django_web_project/project_first_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import CarList
+from .views import CarList, CarCreate
 
 
 urlpatterns = [
     path('owner/<int:owner_id>/', views.owner_detail),
     path('owner_list', views.owner_list),
-    path('car_list', CarList.as_view())
+    path('owner_form', views.create_owner_view),
+    path('car_list', CarList.as_view()),
+    path('car_form', CarCreate.as_view())
 ]
--- students/k3342/practical_works/Salnikova_Nadezhda/simple_django_web_project/project_first_app/views.py
+++ students/k3342/practical_works/Salnikova_Nadezhda/simple_django_web_project/project_first_app/views.py
@@ -1,7 +1,9 @@
 from django.http import Http404
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from project_first_app.models import Owner, Car, DriverLicense, Ownership
+from .forms import OwnerForm
 
 
 def owner_detail(request, owner_id):
@@ -23,6 +25,28 @@ def owner_list(request):
     return render(request, "owner_list.html", context)
 
 
+def create_owner_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context = {}
+
+    form = OwnerForm(
+        request.POST or None)  # создаем экземпляр формы, отсылаем в него данные из формы (из полей в браузере)
+    if form.is_valid():  # Проверка формы на корректность (валидация)
+        form.save()
+    context['form'] = form
+    return render(request, "create_owner_view.html", context)
+
+
 class CarList(ListView):
     # specify the model for list view
     model = Car
+
+
+class CarCreate(CreateView):
+    # specify the model for create view
+    model = Car
+
+    # specify the fields to be displayed
+    fields = ['car_number', 'brand', 'model', 'color']
+    success_url = '/car_form'

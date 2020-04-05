f9f48e14e38b82e876d6ab9e160ef9698d0d882b
Elizabeth <31669496+beliz1729@users.noreply.github.com>
2020-04-05 13:27:45 +0300

    Добавлена задача 3 изПР№2

--- students/k3343/practical_works/Богданова Елизавета/simple_django_web_project/project_first_app/views.py
+++ students/k3343/practical_works/Богданова Елизавета/simple_django_web_project/project_first_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from .models import Car_owner, Car, Driver_license, Owning
+from .forms import Car_ownerForm, CarForm
 
 def detail(request, owner_id):
     try:
@@ -14,6 +16,26 @@ def show_all_owners(request):
     owners={}
     owners["owners"] = Car_owner.objects.all()
     return render(request, 'all_owners_view.html', owners)
+
+def create_view_owners(request):
+    context = {}
+    form = Car_ownerForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context['form'] = form
+    return render (request,"create_view_owners.html", context)
+
+
+class CarsCreate(CreateView):
+    model = Car
+    fields = ['car_make','car_model','car_color','id_car']
+    def as_view(request):
+        autos = {}
+        form = CarForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        autos['form'] = form
+        return render(request, 'create_view_cars.html', autos)
 
 
 class Show_car(ListView):

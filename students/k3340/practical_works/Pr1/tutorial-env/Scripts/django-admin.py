c23041b58ff79d4de3f8d913f96e0fa1cd7fc5e4
arladnice <darlakov@mail.ru>
2020-04-08 01:05:23 +0300

    add .gitignore

--- students/k3340/practical_works/Pr1/tutorial-env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\darla\onedrive\рабочий стол\new\tutorial-env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

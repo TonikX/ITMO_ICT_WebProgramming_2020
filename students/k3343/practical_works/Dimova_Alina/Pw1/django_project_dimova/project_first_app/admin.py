8d676f845dcf048b29361afd59e01da1076a833d
Adelaide <dimova_alina@mail.ru>
2020-04-15 11:30:56 +0300

    Удалена папка с первой работой, результат двух работ в единой папке проекта

--- students/k3343/practical_works/Dimova_Alina/Pw1/django_project_dimova/project_first_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Owner, Auto, Ownership, Licence
-
-admin.site.register(Owner)
-admin.site.register(Auto)
-admin.site.register(Licence)
-admin.site.register(Ownership)

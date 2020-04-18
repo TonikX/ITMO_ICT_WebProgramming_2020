afcb3546ce4552b279cc9b141d05c7c4beb4f7b0
arladnice <darlakov@mail.ru>
2020-04-18 23:19:08 +0300

    Lab_1
    Python Django project for implementing CRUD operations

---
+++ students/k3340/laboratory_works/laboratory_works/Arlakov_Denis/laboratiry_work_1/lab/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\github\web (django)\itmo_ict_webprogramming_2020\students\k3340\laboratory_works\laboratory_works\arlakov_denis\laboratiry_work_1\lab\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

7abb1d615cca498dd644ce93051b76dcfa9e7836
Arladnice <darlakov@mail.ru>
2020-04-18 23:43:52 +0300

    fix gitignore

--- students/k3340/laboratory_works/laboratory_works/Arlakov_Denis/laboratiry_work_1/lab/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\github\web (django)\itmo_ict_webprogramming_2020\students\k3340\laboratory_works\laboratory_works\arlakov_denis\laboratiry_work_1\lab\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

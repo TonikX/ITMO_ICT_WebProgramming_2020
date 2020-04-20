a58132b0bdc68fa276993ac68401c9a732ac25f2
ValeriyaArt <32551040+ValeriyaArt@users.noreply.github.com>
2020-04-20 20:23:49 +0300

    Исправлено по pep8

--- students/k3343/practical_works/Artamonova_Valeriya/simple_django_web_project/project_first_app/views.py
+++ students/k3343/practical_works/Artamonova_Valeriya/simple_django_web_project/project_first_app/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render
 from django.http import Http404
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
-from .models import Auto_owner, Automobile, Driver_license, Owning
-from .forms import Auto_ownerForm, AutomobileForm
+from .models import AutoOwner, Automobile, DriverLicense, Owning
+from .forms import AutoOwnerForm, AutomobileForm
 
 
 def detail(request,owner_id):
     try:
-        auto_owner = Auto_owner.objects.get(pk=owner_id)
-    except Auto_owner.DoesNotExist:
+        auto_owner = AutoOwner.objects.get(pk=owner_id)
+    except AutoOwner.DoesNotExist:
         raise Http404("Auto_owner does not exist")
     return render(request,'auto_owner.html',{'auto_owner':auto_owner})
 
 
 def show_all_owners(request):
     owners={}
-    owners["owners"] = Auto_owner.objects.all()
+    owners["owners"] = AutoOwner.objects.all()
     return render(request, 'all_owners_view.html', owners)
 
 
-class Show_auto(ListView):
+class ShowAutoView(ListView):
     model = Automobile
 
     def as_view(request):
@@ -31,14 +31,14 @@ class Show_auto(ListView):
 
 def create_view_owners(request):
     context = {}
-    form = Auto_ownerForm(request.POST or None)
+    form = AutoOwnerForm(request.POST or None)
     if form.is_valid():
         form.save()
     context['form'] = form
     return render (request,"create_view_owners.html", context)
 
 
-class AutomobilesCreate(CreateView):
+class AutomobilesCreateView(CreateView):
     model = Automobile
     fields = ['car_make','car_model','car_color','id_car']
     def as_view(request):

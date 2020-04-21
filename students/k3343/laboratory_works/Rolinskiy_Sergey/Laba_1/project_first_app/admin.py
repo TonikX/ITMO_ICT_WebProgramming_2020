6d8831fbdcc2d4130f30cd8e4d1a920e08a3dfd9
sergrol <sergrol@gmail.com>
2020-04-21 11:50:03 +0300

    Добавлена часть задания на проверку

---
+++ students/k3343/laboratory_works/Rolinskiy_Sergey/Laba_1/project_first_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from project_first_app.models import *
+# Register your models here.
+admin.site.register(Hotel)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
---
+++ students/k3343/laboratory_works/Rolinskiy_Sergey/Laba_1/project_first_app/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.db import models
+from django.contrib.auth.models import User
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('post','email','name','body','active')
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].label = 'Логин'
+        self.fields['password'].label = 'Пароль'
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_check = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password',
+            'password_check',
+            'first_name',
+            'last_name',
+            'email'
+        ]
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].label = 'Ваш никнейм'
+        self.fields['password'].label = 'Пароль'
+        self.fields['password'].help_text = 'Придумайте пароль'
+        self.fields['password_check'].label = 'Повторите пароль'
+        self.fields['first_name'].label = 'Имя'
+        self.fields['last_name'].label = 'Фамилия'
+        self.fields['email'].label = 'Почта'
+        self.fields['email'].help_text = 'Следует указать действующий адрес'

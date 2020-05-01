0d396809beb041cd11a723638bdc4ea05e3f59aa
ValeriyaArt <32551040+ValeriyaArt@users.noreply.github.com>
2020-05-01 15:31:36 +0300

    Файлы к практической работе3

--- students/k3343/practical_works/Artamonova_Valeriya/simple_django_web_project/manage.py
+++ students/k3343/practical_works/Artamonova_Valeriya/simple_django_web_project/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project_Artamonova.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_django_web_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

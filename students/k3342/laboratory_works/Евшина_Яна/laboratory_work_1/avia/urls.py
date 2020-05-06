87b8f7ecfaf44d44fdf115f9b4f6e60dc7971e06
Yana Evshina <eushina@mail.ru>
2020-05-06 14:57:48 +0300

    Исправлена 1 лабораторная

--- students/k3342/laboratory_works/Евшина_Яна/laboratory_work_1/avia/urls.py
+++ students/k3342/laboratory_works/Евшина_Яна/laboratory_work_1/avia/urls.py
@@ -9,9 +9,9 @@ from .views import LogoutView
 
 
 urlpatterns = [
-    path('all_flights/', views.all_flights),
-    path('flight_info/<int:fly_id>/', views.flight_info),
-    path('register/', views.register),
-    path('login/', LoginFormView.as_view()),
-    path('logout/', LogoutView.as_view()),
+    path('all_flights/', views.all_flights, name='all_flights'),
+    path('flight_info/<int:fly_id>/', views.flight_info, name='flight_info'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginFormView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     ]

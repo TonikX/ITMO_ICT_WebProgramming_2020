c4fbce4e73ec61bdddf2ee6e54ccdd9918263c14
Marika Larionova <marialarionova@gmail.com>
2020-05-23 21:02:18 +0300

    Добавила файлы для второй лабораторной работы

---
+++ students/k3343/laboratory_works/Larionova_Maria/laboratory_work_2/hotel-admin/hotel_admin/hotel/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from .models import *
+
+
+class FloorSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Floor
+		fields = '__all__'
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Room
+		fields = '__all__'
+
+
+class RoomSerializer(serializers.ModelSerializer):
+	room_type = serializers.CharField(source='get_room_type_display')
+	room_state = serializers.CharField(source='get_room_state_display')
+
+	class Meta:
+		model = Room
+		fields = '__all__'
+
+
+class ClientSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Client
+		fields = '__all__'
+
+
+class CreateClientRoomSerializer(serializers.ModelSerializer):
+	client = ClientSerializer()
+	room = CreateRoomSerializer()
+
+	class Meta:
+		model = ClientRoom
+		fields = '__all__'
+
+
+class ClientRoomSerializer(serializers.ModelSerializer):
+	client = ClientSerializer()
+	room = RoomSerializer()
+
+	class Meta:
+		model = ClientRoom
+		fields = '__all__'
+
+
+class CleaningSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Cleaning
+		fields = '__all__'
+
+
+class ChallengerSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Challengers
+		fields = '__all__'
+
+
+class RequestSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Request
+		fields = '__all__'
---
+++ students/k3343/laboratory_works/Larionova_Maria/laboratory_work_2/hotel-admin/hotel_admin/hotel/views.py
@@ -0,0 +1,90 @@
+from rest_framework.response import Response
+from rest_framework import generics
+
+from .models import *
+from .serializers import *
+
+
+class CreateFloorView(generics.CreateAPIView):
+	queryset = Floor.objects.all()
+	serializer_class = FloorSerializer
+
+
+class RetrieveFloorView(generics.RetrieveAPIView):
+	queryset = Floor.objects.all()
+	serializer_class = FloorSerializer
+
+
+class ListFloorView(generics.ListAPIView):
+	queryset = Floor.objects.all()
+	serializer_class = FloorSerializer
+
+
+class CreateRoomView(generics.CreateAPIView):
+	queryset = Room.objects.all()
+	serializer_class = CreateRoomSerializer
+
+
+class RetrieveRoomView(generics.RetrieveAPIView):
+	queryset = Room.objects.all()
+	serializer_class = RoomSerializer
+
+
+class ListRoomView(generics.ListAPIView):
+	queryset = Room.objects.all()
+	serializer_class = RoomSerializer
+
+
+class CreateClientView(generics.CreateAPIView):
+	queryset = Client.objects.all()
+	serializer_class = ClientSerializer
+
+
+class RetrieveClientView(generics.RetrieveAPIView):
+	queryset = Client.objects.all()
+	serializer_class = ClientSerializer
+
+
+class ListClientView(generics.ListAPIView):
+	queryset = Client.objects.all()
+	serializer_class = ClientSerializer
+
+
+class CreateClientRoomView(generics.CreateAPIView):
+	queryset = ClientRoom.objects.all()
+	serializer_class = CreateClientRoomSerializer
+
+
+class RetrieveClientRoomView(generics.RetrieveAPIView):
+	queryset = ClientRoom.objects.all()
+	serializer_class = ClientRoomSerializer
+
+
+class ListClientRoomView(generics.ListAPIView):
+	queryset = ClientRoom.objects.all()
+	serializer_class = ClientRoomSerializer
+
+
+class CreateCleaningView(generics.CreateAPIView):
+	queryset = Cleaning.objects.all()
+	serializer_class = CleaningSerializer
+
+
+class RetrieveCleaningView(generics.RetrieveAPIView):
+	queryset = Cleaning.objects.all()
+	serializer_class = CleaningSerializer
+
+
+class ListCleaningView(generics.ListAPIView):
+	queryset = Cleaning.objects.all()
+	serializer_class = CleaningSerializer
+
+
+class CreateChallengerView(generics.CreateAPIView):
+	queryset = Challengers.objects.all()
+	serializer_class = ChallengerSerializer
+
+
+class CreateRequestView(generics.CreateAPIView):
+	queryset = Request.objects.all()
+	serializer_class = RequestSerializer

b62b3c6dc90bd7b7dc81605aaca368ab77a6097b
ValeriyaArt <32551040+ValeriyaArt@users.noreply.github.com>
2020-05-31 03:21:22 +0300

    Create urls.py

---
+++ students/k3343/laboratory_works/Artamonova_Valeriya/laboratory_work_2/laboratory_work_2/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/', include('school.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

69b80e2ba22dc179548f1fe42502890afcba630a
Алексей <voronoff2803@gmail.com>
2020-06-16 20:38:21 +0300

    lab1 fix

--- students/k3340/laboratory_works/Voronov Alexey/lab1/raceapp/admin.py
+++ students/k3340/laboratory_works/Voronov Alexey/lab1/raceapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from raceapp.models import Result, Vehicle, Person, Comment, Team
+from raceapp.models import Result, Vehicle, Person, Comment, Team, Race
 
 admin.site.register(Result)
 admin.site.register(Vehicle)
 admin.site.register(Person)
 admin.site.register(Comment)
 admin.site.register(Team)
+admin.site.register(Race)
 
--- students/k3340/laboratory_works/Voronov Alexey/lab1/raceapp/forms.py
+++ students/k3340/laboratory_works/Voronov Alexey/lab1/raceapp/forms.py
@@ -1,10 +1,20 @@
 
 # coding=utf-8
 from django.forms import ModelForm
-from .models import Comment
+from .models import Comment, Race, Result
 
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ('text', 'type')
+
+class RaceForm(ModelForm):
+    class Meta:
+        model = Race
+        fields = ('name', 'description')
+
+class ResultForm(ModelForm):
+    class Meta:
+        model = Result
+        fields = ('name', 'description', 'date', 'result', 'vehicle', 'driver', 'race')

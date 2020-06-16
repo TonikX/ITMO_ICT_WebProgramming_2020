9d8fed889a21a7a15644681550c0983c670c8b0f
SprutSDM <zakoylov@gmail.com>
2020-06-16 20:20:08 +0300

    init

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul id="app">
+        <li v-for="(todo, index) in todos" :key="index" @click="deleteTodo(index)">{{ todo }}</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props : [
+            'todos'
+        ],
+        name: 'TodoList',
+        methods: {
+            deleteTodo(index) {
+                this.$emit('delete-todo', index);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

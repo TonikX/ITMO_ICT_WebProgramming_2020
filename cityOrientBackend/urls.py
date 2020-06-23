39d82963b44a200c0001e6378183c2c591e477c6
SprutSDM <zakoylov@gmail.com>
2020-06-23 16:25:13 +0300

    Move auth route

--- cityOrientBackend/urls.py
+++ cityOrientBackend/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('quests/', include('quests.api.endpoints')),
     path('users/', include('users.api.endpoints')),
+    path('auth', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- users/api/endpoints.py
+++ users/api/endpoints.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.api import views
 
 app_name = 'users'
 urlpatterns = [
     path('questMaker', views.QuestMakerListView.as_view(), name='quest-maker-list'),
-    path('team', views.TeamListView.as_view(), name='team-register-list'),
-    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('team', views.TeamListView.as_view(), name='team-register-list')
 ]

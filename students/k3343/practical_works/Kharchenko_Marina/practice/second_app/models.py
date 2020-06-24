a4ea9b61e5907a8b7b88e8e163474f8fff4dde2f
matishakharchen <marishakharchen@gmail.com>
2020-06-25 00:58:22 +0300

    3 практика

---
+++ students/k3343/practical_works/Kharchenko_Marina/practice/second_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    passport_number = models.CharField(max_length=10, blank=True, null=True)
+    home_address = models.CharField(max_length=40, blank=True, null=True)
+    nationality = models.CharField(max_length=40, blank=True, null=True)

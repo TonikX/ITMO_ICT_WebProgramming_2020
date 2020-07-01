b06de9c80322301ba2786d70d108020655792765
Анецка <anya.itmo@yandex.ru>
2020-07-01 15:14:58 +0300

    Добавлен .gitignore

--- students/k3342/laboratory_works/Osipenkova Anna/laboratory_work_2-3/climber/climber_django/venv/lib/python3.7/site-packages/djoser/urls/authtoken.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from djoser import views
-
-urlpatterns = [
-    url(r"^token/login/?$", views.TokenCreateView.as_view(), name="login"),
-    url(r"^token/logout/?$", views.TokenDestroyView.as_view(), name="logout"),
-]

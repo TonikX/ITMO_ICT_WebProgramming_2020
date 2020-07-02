3fdfb5faa4c58e3403be6ef9ce3d43d97f67be16
SprutSDM <zakoylov@gmail.com>
2020-07-02 23:44:49 +0300

    Move code to src folder

--- backend/src/city_orient_backend/asgi.py
+++ backend/src/city_orient_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cityOrientBackend project.
+ASGI config for city_orient_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cityOrientBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'city_orient_backend.settings')
 
 application = get_asgi_application()

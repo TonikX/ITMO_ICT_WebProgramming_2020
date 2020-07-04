fb8647293a4bc688fb641dc39b8095659e19323e
mxmvn <31709265+mxmvn@users.noreply.github.com>
2020-07-04 15:13:01 +0300

    Добавлена практическая работа 2

--- students/k3342/practical_works/Maximov_Nikita/django_project_Maximov/django_project_Maximov/apps/project_first_app/urls.py
+++ students/k3342/practical_works/Maximov_Nikita/django_project_Maximov/django_project_Maximov/apps/project_first_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .views import CarList, AddCar
 
 urlpatterns = [
-	path('owner/<int:owner_id>', views.about_owner)
+	path('owner/<int:owner_id>', views.about_owner),
+	path('owner_list/', views.owner_list),
+	path('car_list/', CarList.as_view()),
+	path('add_owner/', views.add_owner),
+	path('add_car/', AddCar.as_view()),
 	]

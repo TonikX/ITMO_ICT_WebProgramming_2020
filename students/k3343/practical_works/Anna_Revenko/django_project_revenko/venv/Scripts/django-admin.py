15c1c671c7a5726bec8c441efb6cbf08fa1c8add
Anna Revenko <annarevenko3191@yandex.ru>
2020-07-10 18:16:39 +0300

    Удаление ненужных файлов

--- students/k3343/practical_works/Anna_Revenko/django_project_revenko/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\810314\PycharmProjects\django_project_revenko\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

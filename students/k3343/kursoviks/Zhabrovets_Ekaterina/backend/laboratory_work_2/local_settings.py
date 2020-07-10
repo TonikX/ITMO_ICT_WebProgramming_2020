8a0f5d91bea684cd46da3a1d8c38add50bf61414
lumisade99 <zhabrovets1999@rambler.ru>
2020-07-10 17:10:13 +0300

    Анонимизированы данные доступа к БД

--- students/k3343/kursoviks/Zhabrovets_Ekaterina/backend/laboratory_work_2/local_settings.py
+++ students/k3343/kursoviks/Zhabrovets_Ekaterina/backend/laboratory_work_2/local_settings.py
@@ -2,8 +2,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'danceclasses',
-        'USER': 'lumisade',
-        'PASSWORD': 'zhabrKate007',
+        'USER': 'dell',
+        'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '5432',
     }

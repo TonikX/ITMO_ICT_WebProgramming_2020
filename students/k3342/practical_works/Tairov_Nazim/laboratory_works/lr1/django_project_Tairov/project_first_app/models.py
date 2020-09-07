d28f485bc007b5be79a0728c99d0ba12e6f2114d
Nazim Tairov <n.tairov9777@gmail.com>
2020-09-07 19:01:38 +0300

    Practice 3

--- students/k3342/practical_works/Tairov_Nazim/laboratory_works/lr1/django_project_Tairov/project_first_app/models.py
+++ students/k3342/practical_works/Tairov_Nazim/laboratory_works/lr1/django_project_Tairov/project_first_app/models.py
@@ -1,24 +1,27 @@
 from django.db import models
-
-
-class CarOwner(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    date_of_birth = models.DateField(blank=True, null=True)
-
-    def __str__(self):
-        return f'User {self.name}'
-
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 class Car(models.Model):
     car_brand = models.CharField(max_length=50)
     car_model = models.CharField(max_length=50)
     car_color = models.CharField(max_length=50)
     state_number = models.PositiveIntegerField()
-    owned_by = models.ForeignKey(CarOwner, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.car_brand}'
+
+class CarOwner(AbstractUser):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    date_of_birth = models.DateField(blank=True, null=True)
+    passport = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    nationality = models.CharField(max_length=50, blank=True, null=True)
+    car = models.ManyToManyField(Car, through='TemporaryOwner')
+
+    def __str__(self):
+        return f'User {self.name}'
 
 
 class DriverLicense(models.Model):
@@ -31,17 +34,17 @@ class DriverLicense(models.Model):
     license_id = models.PositiveIntegerField()
     date_of_issue = models.DateField(blank=True, null=True)
     choice_type = models.CharField(max_length=5, choices=TYPE_CHOICES)
-    owner = models.ManyToManyField(Car, through='TemporaryOwner', related_name='temp_owner')
+    owner = owner_lic = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.license_id}'
 
 
 class TemporaryOwner(models.Model):
-    car_owner = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name='Car')
-    driver_license = models.ForeignKey(DriverLicense, on_delete=models.CASCADE)
     date_started = models.DateTimeField(blank=True, null=True)
     date_ended = models.DateTimeField(blank=True, null=True)
+    car_owner = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name='Car')
+    owner_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.car_owner} owned by user with license id {self.driver_license}'
